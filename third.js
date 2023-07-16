56ca519be683ca6d20f87dddfacd12183c337c86
Bhaga55 <136149268+Bhaga55@users.noreply.github.com>
2023-07-16 12:47:17 +0530

    Adding third.js

---
+++ third.js
@@ -0,0 +1,8 @@
+var arr =[[1,2,3],[4,5,6],[7,8]];
+var newarr =[];
+for(var i=0;i<arr.length;i++){
+    for(var j=0;j<arr[i].length;j++){
+        newarr.push(arr[i][j]);
+    }
+}
+console.log(newarr)
